c1f4d859ad603a1a9a13432dc74e25af4086a4ea
Hatem Salem <162980382+HatemSal@users.noreply.github.com>
2024-04-25 22:55:47 +0200

    Added reading from file/writing to file functions to StudentClass.h

--- StudentClass.h
+++ StudentClass.h
@@ -1,6 +1,10 @@
 #ifndef STUDENTCLASS_H
 #define STUDENTCLASS_H
 #include<iostream>
+#include<vector>
+#include<fstream>
+#include <chrono>
+// #include"StudentClass.cpp"
 using namespace std;
 class Student{
     string ID;
@@ -11,8 +15,21 @@ public:
     Student(string id, string name, double gpa);
     bool operator > (Student& otherStudent);
     bool operator < (Student& otherStudent);
-    
-friend ostream& operator<<(ostream& os, const Student& student);
+    friend ostream& operator<<(ostream& os, const Student& student);
+    static void modifySortByName(bool flag);
 };
+vector<Student> readFromFile(string fileName);
+template<typename T>
+void SortAndEvaluate(void (*func)(vector<T>&), vector<T> &v, string filename){
+    ofstream file(filename, ios::app);
+    auto start = chrono::high_resolution_clock::now();
+    func(v);
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    file<<"Running Time: "<<duration<<" microseconds"<<endl<<endl;
+    for(auto x:v){
+        file<<x;
+    }
+}
 
 #endif
