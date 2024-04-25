9f48c199cc1cb5171c07889d3f95156b095a82fd
Hatem Salem <162980382+HatemSal@users.noreply.github.com>
2024-04-25 22:53:20 +0200

    Added reading from file, writing to file functions to StudentClass.cpp

--- StudentClass.cpp
+++ StudentClass.cpp
@@ -22,7 +22,37 @@ bool Student::operator < (Student& otherStudent){
     }
 }
 ostream& operator <<(ostream& os, const Student& student) {
-    os << "Name: " << student.Name << ", GPA: " << student.GPA;
+    os <<student.Name<<endl<<student.ID<<endl<<student.GPA<<endl<<endl;
     return os;
 }
-
+void Student::modifySortByName(bool flag){
+    sortByName = flag;
+}
+bool Student::sortByName;
+vector<Student> readFromFile(string fileName){
+    vector<Student> students;
+    ifstream studentsFile(fileName);
+    if(!studentsFile.is_open()){
+        cout<<"error opening the file";
+        return students;
+    }
+    string dummy;
+    int numStudents;
+    studentsFile>>numStudents;
+    getline(studentsFile,dummy);
+    for(int i =0; i<numStudents; i++){
+        string name;
+        string id;
+        string gpas;
+        getline(studentsFile,name);
+        getline(studentsFile,id);
+        getline(studentsFile,gpas);
+        name.pop_back();
+        id.pop_back();
+        gpas.pop_back();
+        double gpa = stod(gpas);
+        Student newStudent(id, name, gpa);
+        students.push_back(newStudent);
+    }
+    return students;
+}
