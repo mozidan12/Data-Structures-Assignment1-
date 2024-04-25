743e4cc7a792bb9ffc78d3a0c74c6044ba069eb9
Hatem Salem <162980382+HatemSal@users.noreply.github.com>
2024-04-25 23:01:56 +0200

    added bubble/selection/insertion sorts to ShellSortUsingFilesDemo.cpp

--- ShellSortUsingFilesDemo.cpp
+++ ShellSortUsingFilesDemo.cpp
@@ -21,6 +21,49 @@ void ShellSort(vector<T> &v){
     }
 }
 
+template<typename T>
+void bubbleSort(vector<T> &v){
+    int n = v.size();
+    for (int i = 0; i < n-1; ++i) {
+        bool flag = false;
+        for (int j = 0; j < n-i-1; ++j) {
+            if(v[j] > v[j+1]) {
+                swap(v[j], v[j + 1]);
+                flag = true;
+            }
+        }
+        if(!flag) // array is sorted
+            break;
+    }
+}
+
+template<typename T>
+void selection_sort(vector<T> &v){
+    int n = v.size();
+    for (int i = 0; i < n-1; ++i) {
+        int minIdx = i;
+        for (int j = i+1; j < n; ++j) {
+            if(v[j] < v[minIdx])
+                minIdx = j;
+        }
+        swap(v[i], v[minIdx]);
+    }
+}
+
+template<typename T>
+void insertion_sort(vector<T> &v){
+    int n = v.size();
+    for (int i = 1; i < n; ++i) {   // 40, 50, 60, 30, 10, 20
+        T key = v[i]; //60
+        int j = i-1;  //1
+        while(j >= 0 && v[j] > key){
+            v[j+1] = v[j];
+            j -= 1;
+        }
+        v[j+1] = key;
+    }
+}
+
 int main()
 {
     // Outputting ShellSort sort by name Results
