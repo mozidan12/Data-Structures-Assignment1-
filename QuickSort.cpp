45d4268a7e8f0f96c510f4e21729cf352ff341d5
Abdelrahman Ahmed Farouk <abdelrahman.ahmed.63@outlook.com>
2024-04-26 19:42:05 +0300

    Updated Quick Sort Algorithm

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -2,43 +2,6 @@
 #include <vector>
 //==================================
 template<typename t>
-void QuickSort(std::vector<t> &v, int FirstIndex, int LastIndex){
-
-    int TempLeft = FirstIndex;
-    int TempRight = LastIndex;
-
-    // Get the pivot element (middle element) 
-    t pivot = v[(FirstIndex + LastIndex) / 2];
-
-
-    while(FirstIndex <= LastIndex){
-
-        // Loop throught the vector until we find an element that is smaller than the pivot
-        while(v[FirstIndex] < pivot)
-            FirstIndex++;
-
-        // Loop throught the vector until we find an element that is greater than the pivot
-        while(v[LastIndex] > pivot)
-            LastIndex--;
-
-        // if the two indexes are still in the right order, swap the elements
-        if(FirstIndex <= LastIndex){
-            std::swap(v[FirstIndex], v[LastIndex]);
-            FirstIndex++;
-            LastIndex--;
-        }
-    }
-
-    // Recursively call the function to sort the left and right vectors
-    if(TempLeft < LastIndex)
-        QuickSort(v, TempLeft, LastIndex);
-
-    if(FirstIndex < TempRight)
-        QuickSort(v, FirstIndex, TempRight);
-
-}
-//==================================
-template<typename t>
 void QuickSort(std::vector<t>& v) {
 
     //Base case
@@ -75,14 +38,7 @@ void QuickSort(std::vector<t>& v) {
 }
 //==================================
 int main(){
-    std::vector<int>v{1, 12, 5, 26, 7, 14, 3, 7, 2};
-    QuickSort(v,0,v.size()-1);
-    for(int i = 0; i < v.size(); i++)
-        std::cout << v[i] << " ";
-
-    std::cout << std::endl;
-//==================================
-    v = {1, 12, 5, 26, 7, 14, 3, 7, 2};
+    std::vector<int> v = {1, 12, 5, 26, 7, 14, 3, 7, 2};
     QuickSort(v);
     for(int i = 0; i < v.size(); i++)
         std::cout << v[i] << " ";
